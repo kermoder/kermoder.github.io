7e53a126e0f8b39e8539a5ccba077be8c6d68f44
robert kermode <kermoder@gmail.com>
2021-08-31 21:43:44 -0400

    Adding youtube

---
+++ ps/component/youtube.js
@@ -0,0 +1,28 @@
+export var youtube = {
+    data: function(){
+    	return {
+    	};
+    },
+    methods: {
+    },
+    template: `
+		<section class="pt-container-small">
+			<div>
+				<p>
+					A video presentation in three parts of the ideas here is available on youtube:
+					<ol>
+						<li>
+							Part 1: <a href="https://youtu.be/WLsbhpnDk4E" target="_blank">the intro</a>
+						</li>	
+						<li>
+							Part 2: <a href="https://youtu.be/Wkffr81tFB4" target="_blank">the model</a> 
+						</li>	
+						<li>
+							Part 3: <a href="https://youtu.be/6QbPfgnWT0o" target="_blank">the definition</a>
+						</li>							
+					</ol>
+				</p>
+			</div>
+		</section>
+	`
+};
