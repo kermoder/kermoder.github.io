78e4a2e7ed35fc7d5ab6d462c2c7ba5bfe2aae03
robert kermode <kermoder@gmail.com>
2021-09-02 22:36:53 -0400

    ok

--- ps/component/who.js
+++ ps/component/who.js
@@ -10,11 +10,8 @@ export var who = {
 		<section class="pt-container-small">
 			<div>
 				<p>
-		           I am a computer programmer, living in Toronto. I became a programmer mainly because my interest in life has always been to understand the nature of thinking, and I figured the best way to work out ideas would be to write programs to implement them - I wanted to work on what most people call artificial intelligence. I have a pile of thoughts on the subject, totally unrelated to what\'s here, and of probably about the same value.
+					My name is Robert Kermode. I like the sound of my own voice, and I think you should, too. I can be reached at kermoder@gmail.com
 				</p>
-				<p>
-		            Before taking up programming I studied philosophy. I was ambivalent about the value of the discipline and also not very good at it. Whether these two conditions are related as cause and effect, and if so how, I cannot say.
-		        </p>
 			</div>
 		</section>
 		`
