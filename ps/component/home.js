b830cb04f7d7e479b2fe00e47f589ffc9a0104d2
robert kermode <kermoder@gmail.com>
2022-03-03 23:23:06 -0500

    ok

--- ps/component/home.js
+++ ps/component/home.js
@@ -8,33 +8,18 @@ export var home = {
     template: `
 		<section class="pt-container">
 			<div>
-				<div class="heading0">
-					tl;dr:
-				</div>
-				<div class="heading1">
-					The situation
-				</div>				
-  		        <p>
-					Familiar treatments of semantics start with words and the things in the world they refer to - "Fido" and Fido, "is a dog" and the property of being a dog. They endeavor to build from there up to sentence meanings.
-				</p>
-				<p>
-					Problems arise: about the nature of the reference relation, the nature of propositions, the practicability of compositional semantics, and so on.
-				</p>
-				<div class="heading1">
-					The thought experiment
-				</div>				
-				<p>
-					Consider a world where people assign scalar values to apparently inert, sentence-like strings of sounds, and sometimes get utility (pleasure) from highly-valued ones. Suitably elaborated and constrained, this world would naturally give rise to a contract between speakers for the exchange of such strings. The ideal to which the contract would bind speakers,it turns out, has the properties of truth. Further constraints and qualifications permit recognizing words and defining sameness of meaning in terms of substitutability <i>salva veritate</i>, and from there, sameness of proposition.
-				</p>
-				<div class="heading1">
-					The insight
-				</div>
-				<p>						
-					If reasonably elaborated and constrained, the sound strings in this thought experiment come naturally to be characterisable in terms of concepts just like the concepts of natural-language semantics. 
-				</p>
-				<p>
-					In sum: the explication of semantics does not require things in the world.
-				</p>
+		      <span class="italic subway">Mind: the gap</span> is a project dedicated to explaining ('explicating') one big idea.
+
+It is widely thought that our ability to understand a language is explained by our grasp of its words' ' meanings and knowledge of its grammar.
+
+The big idea is that this thought gets matters roughly upside-down. Once set upright, a lot of things - truth, meaning, even consciousness, become much clearer. But the world looks a lot different.
+
+
+
+
+The work we imagine such theorizing to do is done by <neural network theories>
+
+
 			</div>
 		</section>`
 };
