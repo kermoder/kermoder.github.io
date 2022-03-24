a7b94731082bf231c9d473bd0e688df9083edcb2
robert kermode <kermoder@gmail.com>
2022-03-23 21:12:50 -0400

    ok

--- ps/component/home.js
+++ ps/component/home.js
@@ -8,18 +8,19 @@ export var home = {
     template: `
 		<section class="pt-container">
 			<div>
-		      <span class="italic subway">Mind: the gap</span> is a project dedicated to explaining ('explicating') one big idea.
+		<p>
+		      <span class="italic subway">Mind: the gap</span> is a project dedicated to one big idea.
+		</p>
+		<!-- p>
+It is widely thought that our reactions to the things people say is explained by our grasp of their words&#39; meanings and our knowledge of the grammar of our shared language.
+		</p -->
+		<p>
+The big idea is that this thought gets matters roughly upside-down. <The confidence we have in what people say understood one way is explained by neuroscience without help from concepts like meaning; understood another way, it is a foundation on which to build our understanding of truth and meaning.>  Once set upright, a lot of things - truth, meaning, even consciousness, become much clearer. The gap in our understanding of the mind is closed. But the world looks a lot different.
+		</p>
+		<p>
+		
 
-It is widely thought that our ability to understand a language is explained by our grasp of its words' ' meanings and knowledge of its grammar.
-
-The big idea is that this thought gets matters roughly upside-down. Once set upright, a lot of things - truth, meaning, even consciousness, become much clearer. But the world looks a lot different.
-
-
-
-
-The work we imagine such theorizing to do is done by <neural network theories>
-
-
+	   </p>
 			</div>
 		</section>`
 };
