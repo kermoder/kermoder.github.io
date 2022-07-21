9c4e6a51cf282e58ed79e8266b00287129db4075
robert kermode <kermoder@gmail.com>
2022-07-20 23:02:31 -0400

    ok

--- ps/component/home.js
+++ ps/component/home.js
@@ -8,17 +8,79 @@ export var home = {
     template: `
 		<section class="pt-container">
 		<p>
-		      <span class="italic subway">Mind: the gap</span> is a project dedicated to one big idea.
+  Much of Analytic philosophy is premised on a fundamental, unquestioned assumption. Expressed as a slogan, it is this:
+		</p>
+		<p style="font-style:italic;">		
+  Things precede facts.
 		</p>
 		<p>
-It is widely thought that our reactions to the things people say is explained by our grasp of their words&#39; meanings and our knowledge of the grammar of our shared language.
+  That's pretty cryptic, I admit. What do I mean? 
 		</p>
 		<p>
-The big idea is that this thought gets matters roughly upside-down. The confidence we have in what people say understood one way is explained by neuroscience without help from concepts like meaning; understood another way, it is a foundation on which to build our understanding of truth and meaning.  Once set upright, a lot of things - truth, meaning, even consciousness, become much clearer. The gap in our understanding of the mind is closed. But the world looks a lot different.
+  I know people like my brother-in-law or my barber, places, like Thunder Bay or Madrid, or even in some sense abstract things like the Greek alphabet say, the characteristic silhouette of the Jaguar E-Type. (what blue or saltiness or softness is)
+  		</p>
+		<p>
+  But we also - differently - know *that* ..., that 
+		</p>
+		<p>
+  This is the contrast I'm after - knowledge of things, and knowledge of propositions which state facts. 
+		</p>
+		<p>
+  My slogan encapsulates the - very natural - thought that our epistemic journey in life begins with the first kind of knowledge, of things, and proceeds from there to knowledge of propositions.
+		</p>
+		<p>
+  This project explores the implications of renouncing this assumption.
+		</p>
+		<p>
+		1. Assumption sets us up to fail.
+		2. Assumption makes knowing a 
+		</p>
+		<p>
+
 		</p>
 		<p>
 		
-
 	   </p>
 		</section>`
 };
+
+/*
+
+  Much of Analytic philosophy is premised on a fundamental, unquestioned assumption. Expressed as a slogan, it is this:
+
+  Things precede facts.
+
+  That's pretty cryptic, I admit. What do I mean? 
+
+  I know people like my brother-in-law or my my barber, places, like Thunder Bay or Madrid, or even in some sense abstract things like the greek alphabet say, the characteristic silhouette of the Jaguar E type. (what blue or saltiness or softness is)
+  
+  But we also - differently - know *that* ..., that 
+
+  This is the contrast I'm after - knowledge of things, and knowledge of propositions which state facts. 
+
+  My slogan encapsulates the - very natural - thought that our epistemic journey in life begins with the first kind of knowledge, of things, and proceeds from there to knowledge of propositions.
+
+  This project explores the implications of renouncing this assumption.
+
+
+  Our brains and minds are finite. Nevertheless, we are capable of expressing and understanding an effectively infinite variety of different thoughts. How can this be? The answer in philosophy is this: we are acquainted with a limited range of things and the qualities they can have, and know a corresponding limited range of words to name the things and their qualities. We are also equipped with a finite set of grammatical rules for combining these words into sentences. But put together, these things - our words and rules of grammar - conspire to give us an effective infinity of possible thoughts.
+  
+This answer is seductive in its simplicity. It is also mistaken. There is a much, much better way of understanding how language, meaning, and ultimately our minds' relation to the world, work. 
+
+The purpose of this site is to explain, in clear, straight-forward terms, this better way. The outcome of the investigation is this:
+
+* Propositions -the contents of thoughts - are just sets of sentences (token sentences) whose words match in their (token) meanings. That is *all* they are.
+
+* Two words have the same meaning just in case they can be substituted in all relevant token sentences without altering the truth of the re-worded sentences. That is *all* there is to meaning. Spelling out that 'relevant' is the crux of the biscuit.
+
+
+
+
+
+
+  
+
+
+
+
+*/
