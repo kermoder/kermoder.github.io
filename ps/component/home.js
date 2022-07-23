9175a0d699dc39c6f87929c3d0123f371c45bd68
robert kermode <kermoder@gmail.com>
2022-07-23 00:59:59 -0400

    ok

--- ps/component/home.js
+++ ps/component/home.js
@@ -8,43 +8,92 @@ export var home = {
     template: `
 		<section class="pt-container">
 		<p>
-  Much of Analytic philosophy is premised on a fundamental, unquestioned assumption. Expressed as a slogan, it is this:
+  Much of philosophy is premised on a fundamental, often unquestioned assumption. Expressed as a slogan, it is this:
 		</p>
-		<p style="font-style:italic;">		
+		<p class="slogan">		
   Things precede facts.
 		</p>
 		<p>
-  That's pretty cryptic, I admit. What do I mean? 
+  That's pretty concise, I admit. What do I mean? 
 		</p>
 		<p>
-  I know people like my brother-in-law or my barber, places, like Thunder Bay or Madrid, or even in some sense abstract things like the Greek alphabet say, the characteristic silhouette of the Jaguar E-Type. (what blue or saltiness or softness is)
+  We are all acquainted with lots of particular people and things. I know people, like my brother-in-law or my barber. I know places and objects, like Thunder Bay or Madrid or my bike pump or my dog's collar. And I know even in some sense abstract things like the Greek alphabet.
+		</p>
+		<p>
+  In a similar way, we're acquainted with properties things have - blueness or saltiness or softness, or what have you.
   		</p>
 		<p>
-  But we also - differently - know *that* ..., that 
+  But we also - differently - have much knowledge <span class="italic">that</span>. We know such things as that Justin is Sophie's husband, or that water is H<sub>2</sub>O. 
 		</p>
 		<p>
-  This is the contrast I'm after - knowledge of things, and knowledge of propositions which state facts. 
+This is the contrast I'm after - knowledge of things versus knowledge of facts or maybe of propositions which state facts. 
 		</p>
 		<p>
-  My slogan encapsulates the - very natural - thought that our epistemic journey in life begins with the first kind of knowledge, of things, and proceeds from there to knowledge of propositions.
+  My slogan encapsulates the - very natural - thought that our epistemic journey in life begins with the first kind of knowledge, of things, and proceeds only from there to knowledge of propositions.
 		</p>
 		<p>
   This project explores the implications of renouncing this assumption.
 		</p>
-		<p>
+		<!-- p>
 		1. Assumption sets us up to fail.
 		2. Assumption makes knowing a 
+		</p -->
+		<p>
+  Up-ending this way of thinking is not difficult, but requires accepting in our thinking some quite foreign -some would no doubt say ridiculous- thoughts:
 		</p>
 		<p>
-
+1. Primacy of sentences: To begin with, we remove from their place of epistemic priority objects and our relation to them and put in their place as epistemically given, sentences (particular, 'token' sentences) and our valuations of them. Sentence valuation is ultimately to be understood as confidence or degree of belief, but all that's needed to be getting on are two distinguishable feelings and a continuum of cases between them.		
 		</p>
 		<p>
-		
+2. Sentences correlate to pleasures/pains. We recognize that some evaluable sentences correlate in repeating ways with the occurrence of familiar mundane good and bad experiences (sentence valuations like money; the mundane experiences, what money can buy) Ex.: There's ice cream in the freezer.
+		</p>
+		<p>
+3. Impredicativity of valuation. We notice that whether or not we value a given token sentence may depend in part on what other token sentences we value. This introduces a gap between what we in fact value and what we <em>ought</em> to value, to maximize overall sentence value and hence pleasure.
 	   </p>
+		<p>
+4. Propositional knowledge is acquired in a community. We notice that the project of collecting valuable sentences to increase pleasures got and pains avoided, is orders of magnitude more effective when done communally (again, the financial analogy)
+		</p>
+		<p>
+5. Competition and collaboration in balance. Each of us wants to maximize personal sentence value. We both collaborate and compete to this end, in our exchange of sentences.
+		</p>
+		<p>
+6. Truth is what maximizes value. A token sentence is true just in case it's an element of a set which maximizes total sentence value of all speakers. In saying a sentence is true, this is all we're saying.
+		</p>
+		<p>
+7. Truth prior to meaning. Sentence truth is thus defined without reference to word-meaning
+		</p>
+		<p>
+8. Given truth, (token) word meaning can be defined in terms of substitutivity salva veritate in token sentences.
+		</p>
+		<p>
+9. Propositions as sets of sentences. Propositions are just sets of token sentences whose words can be paired off in such a way that the words of each pair are synonyms.
+		</p>
 		</section>`
 };
 
 /*
+
+Up-ending this way of thinking is not difficult, but requires accepting in our thinking in some quite foreign -some would no doubt say ridiculous- thoughts:
+
+1. Primacy of sentences: To begin with, we remove from its place of epistemic priority our relation to objects, and put in its place as epistemically given, sentences (token sentences) heard and our valuations of them. Sentence valuation is ultimately to be understood as confidence or degree of belief, but all that's needed to be getting on is two distinguishable feelings and a continuum of cases between them.
+
+2. Sentences correlate to pleasures/pains. We recognize that some evaluable sentences correlate in repeatable ways with the occurrence of familiar mundane good and bad experiences (sentence valuations like money; the mundane experiences, what money can buy)
+
+3. Propositional knowledge is acquired in a community. We notice that the project of collecting valuable sentences to increase pleasures got and pains avoided, is orders of magnitude more effective when done communally (again, the financial analogy)
+
+4. Impredicativity. We stipulate that 
+
+5. Truth: this gets us to truth of sentences
+
+6. Truth is prior to meaning
+
+7. (token word ...) Meaning can be defined in terms of substitutivity salva veritate
+
+8. propositions can be defined in terms of word meanings - sets of token sentences.
+
+
+
+
 
   Much of Analytic philosophy is premised on a fundamental, unquestioned assumption. Expressed as a slogan, it is this:
 
